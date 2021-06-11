6293e3b8483a500e3972b0f8c7ccbd0d659f3f94
brianschhh <brian.schmi@hotmail.com>
2021-06-11 00:31:14 -0300

    final

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ class Cart {
   }
 
   addProduct(product) {
-    this.products.push(product)
+    const isRepeat = this.products.findIndex(item => item.id === product.id)
+    if (isRepeat >= 0) {
+      this.products[isRepeat] = {
+        ...product,
+        quantity: this.products[isRepeat].quantity + 1
+      }
+    } else {
+      this.products.push(product)
+    }
   }
 
   deleteProduct(id) {
@@ -16,26 +24,24 @@ class Cart {
     let total = 0
 
     this.products.forEach(product => {
-      total = total + parseFloat(product.price)
+      total = total + parseFloat(product.price) * parseInt(product.quantity)
     })
 
     return total
   }
-
-  
 }
 
 class Product {
-  constructor(id, name, price, image, ) {
+  constructor(id, name, price, image) {
     this.id = id
     this.name = name
     this.price = price
     this.image = image
-    
+    this.quantity = 1
   }
 }
 
-let cart = new Cart()
+const cart = new Cart()
 
 const products = document.querySelectorAll('.product')
 
@@ -45,70 +51,57 @@ products.forEach(product => {
   const image = product.querySelector('.image').getAttribute('src')
   const price = product.querySelector('.price').innerText
   const id = product.querySelector('.id').getAttribute('value')
-  
 
-  
-  
-/**
+  /**
    * Agregar producto al carrito y dibujar en el html
    */
   button.addEventListener('click', () => {
-    const displayCart = document.getElementById('cart')
-
-    const product = new Product(id, name, price, image, )
+    const product = new Product(id, name, price, image)
     cart.addProduct(product)
-  
 
     const totalPrice = cart.calculateTotal()
     const totalPriceText = document.querySelector('.total-price')
     totalPriceText.innerText = totalPrice
 
-    
-
-    // crear bloque html
-    const articleContainer = document.createElement('article')
-    const imgContainer = document.createElement('img')
-    const dataContainer = document.createElement('div')
-    const nameContainer = document.createElement('h2')
-    const priceContainer = document.createElement('h3')
-    const deleteButton = document.createElement('span')
-
-
-    nameContainer.innerText = name
-    priceContainer.innerText = '$' + price
-    imgContainer.src = image
-    deleteButton.innerText = 'x'
-    
-
     const productList = document.querySelector('.products-list')
+    productList.innerHTML = ''
 
-    articleContainer.appendChild(imgContainer)
-    articleContainer.appendChild(dataContainer)
-    dataContainer.appendChild(nameContainer)
-    dataContainer.appendChild(priceContainer)
-    dataContainer.appendChild(deleteButton)
-    productList.appendChild(articleContainer)
-    
-   
+    cart.products.forEach(item => {
+      // crear bloque html
+      const articleContainer = document.createElement('article')
+      const imgContainer = document.createElement('img')
+      const dataContainer = document.createElement('div')
+      const nameContainer = document.createElement('h2')
+      const priceContainer = document.createElement('h3')
+      const deleteButton = document.createElement('span')
 
-    const buttonAllDelete = document.querySelector('.buttonDeleteAll')
-    buttonAllDelete.addEventListener('click', () => {
-      articleContainer.remove()
-      cart = new Cart()
-      totalPriceText.innerText = 0
+      nameContainer.setAttribute('id', 'item-' + item.id)
+
+      nameContainer.innerText = item.name + 'x' + item.quantity
+      priceContainer.innerText = '$' + item.price
+      imgContainer.src = item.image
+      deleteButton.innerText = 'x'
+
+      articleContainer.appendChild(imgContainer)
+      articleContainer.appendChild(dataContainer)
+      dataContainer.appendChild(nameContainer)
+      dataContainer.appendChild(priceContainer)
+      dataContainer.appendChild(deleteButton)
+      productList.appendChild(articleContainer)
+
       
+
       
 
-    })
+      deleteButton.addEventListener('click', () => {
+        cart.deleteProduct(item.id)
+        articleContainer.remove()
 
-
-    deleteButton.addEventListener('click', () => {
-      cart.deleteProduct(id)
-      articleContainer.remove()
-
-      const totalPrice = cart.calculateTotal()
-      const totalPriceText = document.querySelector('.total-price')
-      totalPriceText.innerText = totalPrice
+        const totalPriceText = document.querySelector('.total-price')
+        totalPriceText.innerText = cart.calculateTotal()
+      })
     })
   })
 })
+
+
